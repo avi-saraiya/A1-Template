914238a11dffa3bf69feb80f7ffd31577375441f
Avi Saraiya <saraiyaa@mcmaster.ca>
2025-04-03 16:05:36 -0400

    Closes #19: Implemented singleton for Maze

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -7,8 +7,24 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Maze implements MazeInterface {
-    private char[][] grid; //2d representation of the maze
+
+    private static Maze instance;  // Private static instance for Singleton
+    private char[][] grid;         // 2D representation of the maze
     private List<String> rows = new ArrayList<>();
+
+    // Private constructor to prevent direct instantiation
+    private Maze() {
+    }
+
+    // Public static method to provide access to the instance
+    public static Maze getInstance(String filePath) throws IOException {
+        if (instance == null) {
+            // If the instance is null, load the maze and create the instance
+            instance = new Maze();
+            instance.loadMazeFromFile(filePath); // Load maze data from the file
+        }
+        return instance;  // Return the single instance
+    }
 
     // Not implemented in the final version
     @Override
@@ -38,7 +54,7 @@ public class Maze implements MazeInterface {
         }
         reader.close();
         this.rows = rowList;
-        this.grid = new char[rowList.size()][rowList.get(0).length()];// Converts rows to the grid
+        this.grid = new char[rowList.size()][rowList.get(0).length()];  // Converts rows to the grid
         for (int i = 0; i < rowList.size(); i++) { // To initialize the grid with maze data
             this.grid[i] = rowList.get(i).toCharArray();
         }
--- src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeLoader.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeLoader.java
@@ -13,7 +13,7 @@ class MazeLoader {
     // Method for loadin in the maze
     public Maze loadMaze() throws Exception {
         BufferedReader reader = new BufferedReader(new FileReader(filePath));
-        Maze maze = new Maze();
+        Maze maze = Maze.getInstance(filePath);
         // Loads the maze using the new method that loads the grid and rows
         maze.loadMazeFromFile(filePath);
         return maze;
