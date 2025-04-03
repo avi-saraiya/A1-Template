7e64a68d93fe6fcabea4e68c6cbc37df1605e6b3
Avi Saraiya <saraiyaa@mcmaster.ca>
2025-04-03 10:09:27 -0400

    Initialized command caller

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Caller.java
@@ -0,0 +1,13 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+public class Caller {
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void call() {
+        command.execute();
+    }
+}
