3facdbc39710a33b18133ebd6e6350b473437a5d
Avi Saraiya <saraiyaa@mcmaster.ca>
2025-04-04 09:47:30 -0400

    added test for multiple commands

--- src/test/java/ca/mcmaster/se2aa4/mazerunner/CallerTest.java
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/CallerTest.java
@@ -20,4 +20,17 @@ public class CallerTest {
         Caller caller = new Caller();
         caller.call();
     }
+
+    @Test
+    public void testMultipleCommandsInCaller() throws Exception {
+        Maze maze = Maze.getInstance("/root/A1-Template/examples/tiny.maz.txt");
+        PathFinder pathFinder = new PathFinder(maze);
+        MoveCommand moveCommand1 = new MoveCommand(pathFinder);
+        MoveCommand moveCommand2 = new MoveCommand(pathFinder);
+        Caller caller = new Caller();
+        caller.setCommand(moveCommand1);
+        caller.call();
+        caller.setCommand(moveCommand2);
+        caller.call();
+    }
 }
