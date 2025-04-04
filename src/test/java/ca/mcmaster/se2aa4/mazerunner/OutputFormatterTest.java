2dfdb3db46f6ef9f4f92eb7fee59608615d4db16
Avi Saraiya <saraiyaa@mcmaster.ca>
2025-04-04 09:54:29 -0400

    Added test for factorized output

--- src/test/java/ca/mcmaster/se2aa4/mazerunner/OutputFormatterTest.java
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/OutputFormatterTest.java
@@ -10,6 +10,14 @@ public class OutputFormatterTest {
         OutputFormatter outputFormatter = new OutputFormatter();
         List<String> moves = List.of("R", "F", "L", "F");
         StringBuilder canonicalForm = outputFormatter.solFormat(moves);
-        assertNotNull(canonicalForm, "Canonical form should not be null.");
+        assertNotNull(canonicalForm, "Canonical form shouldn't be null.");
+    }
+
+    @Test
+    public void testOutputFormatterForFactorizedForm() {
+        OutputFormatter outputFormatter = new OutputFormatter();
+        StringBuilder canonicalForm = new StringBuilder("R F L F");
+        StringBuilder factorizedForm = outputFormatter.factorize(canonicalForm);
+        assertNotNull(factorizedForm, "Factorized form shouldn't be null.");
     }
 }
